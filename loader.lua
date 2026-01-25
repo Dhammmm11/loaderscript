8409b84e158308592f516b027cc0a2159343d0d8
Dhammmm11 <dhamargaming01@gmail.com>
2026-01-25 09:33:08 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,15 +1,9 @@
--- Daftar Game dan Script-nya (Format Table seperti punya temanmu)
 local GameScripts = {
-    -- [ID GAME] = "LINK SCRIPT"
-    
-    -- Violence District
+
     [93978595733734] = "https://raw.githubusercontent.com/Dhammmm11/violence-district/main/main.lua",
     
-    -- Poop a Big Poop
     [85050171250159] = "https://raw.githubusercontent.com/Dhammmm11/poop-a-big-poop-script/main/source.lua"
 }
-
--- == JANGAN UBAH KODE DI BAWAH INI == --
 
 local PlaceId = game.PlaceId
 local scriptUrl = GameScripts[PlaceId]
@@ -18,8 +12,7 @@ print("-------------------------------------")
 print("Place ID Saat Ini: " .. PlaceId) 
 
 if scriptUrl then
-    print("✅ Game Terdaftar! Memuat Script...")
-    -- Fungsi pcall untuk menangkap error jika link mati
+    print("✅ Game Terdaftar! Memuat Script...")    
     local success, err = pcall(function()
         loadstring(game:HttpGet(scriptUrl))()
     end)
