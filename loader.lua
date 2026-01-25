c5c20e7d7e35a28d7f19f75669b20c3654747db7
Dhammmm11 <dhamargaming01@gmail.com>
2026-01-25 09:12:59 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,20 +1,18 @@
-local PlaceId = game.PlaceId
+local PlaceId = game.PlaceId 
 local Game_ViolenceDistrict = 93978595733734 
 local Game_PoopAbigPoop = 85050171250159      
+
 print("Terdeteksi Place ID: " .. PlaceId)
+
 if PlaceId == Game_ViolenceDistrict then
     print("Loading Script Violence District...")
-    
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhammmm11/violence-district/refs/heads/main/main.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhammmm11/violence-district/main/main.lua"))()
 
 elseif PlaceId == Game_PoopAbigPoop then
-    print("Loading Script Blade Ball...")
-  
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhammmm11/poop-a-big-poop-script/refs/heads/main/source.lua"))()
+    print("Loading Script Poop Game...")
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Dhammmm11/poop-a-big-poop-script/main/source.lua"))()
 
 else
-
     print("Game tidak terdaftar di Hub. Memuat Universal Script...")
-    
     loadstring(game:HttpGet("https://raw.githubusercontent.com/EdgeIY/infiniteyield/master/source"))()
 end
