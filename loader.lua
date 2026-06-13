7d8aeecaa401d0d8b3d48c7bc149a87927d73eca
Dhammmm11 <dhamargaming01@gmail.com>
2026-06-14 06:50:17 +0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,7 +1,7 @@
 local GameScripts = {
     [93978595733734] = "https://raw.githubusercontent.com/Dhammmm11/violence-district/main/main.lua",
     [85050171250159] = "https://raw.githubusercontent.com/Dhammmm11/poop-a-big-poop-script/main/source.lua",
-    [122446657157717] = "https://raw.githubusercontent.com/Dhammmm11/sniperarena/main/main.lua",
+    [122446657157717,119661268047775] = "https://raw.githubusercontent.com/Dhammmm11/sniperarena/main/main.lua",
 }
 
 local PlaceId = game.PlaceId
